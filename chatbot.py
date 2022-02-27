d6504d4bd2e3187d24e949a4070af1f8ef464012
Htick <1518737912@qq.com>
2022-02-27 12:34:45 +0800

    更新代码

--- chatbot.py
+++ chatbot.py
@@ -1,30 +1,46 @@
-import telegram
-from telegram.ext import Updater, MessageHandler, Filters
-# The messageHandler is used for all message updates
-# messageHandler 用于所有消息更新
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
 import configparser
 import logging
+import redis
+
+global redis1
 
 
 def main():
     # Load your token and create an Updater for your Bot
-    #  加载你的令牌并为你的机器人创建一个更新程序
+    # 加载令牌并创建机器人
+
     config = configparser.ConfigParser()
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
+
+    # 建立数据库连接
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']),
+                         port=(config['REDIS']['REDISPORT']))
+
     # You can set this logging module, so you will know when and why things do not work as expected
     # 你可以设置这个日志模块，这样你就会知道什么时候以及为什么事情没有按预期工作
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
-    
+
     # register a dispatcher to handle message: here we register an echo dispatcher
     # 注册一个dispatcher来处理消息：这里我们注册一个echo dispatcher
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
 
+    # on different commands - answer in Telegram
+    # 在不同的命令上 - 在 Telegram 中回答
+
+    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler(('hello', 'Kevin', 'hello_kevin'), hello))
+
     # To start the bot:
-    # 启动机器人：
+    # 启动机器人
     updater.start_polling()
     updater.idle()
 
@@ -34,6 +50,31 @@ def echo(update, context):
     logging.info("Update: " + str(update))
     logging.info("context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+# 定义一些命令处理程序。 这些通常采用两个参数更新和上下文。 错误处理程序也会错误地接收引发的 TelegramError 对象。
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Helping you helping you.')
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Good day, Kevin!')
+
+
+def add(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /add is issued."""
+    try:
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0]  # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+        update.message.reply_text('You have said ' + msg + ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <keyword>')
+
 
 if __name__ == '__main__':
     main()
